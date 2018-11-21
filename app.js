b4a176f13da9aa73d9edf29a24e5f534bbdef72b
sylvestertarnowski <sylwek.tarnowski@gmail.com>
2018-11-21 06:53:09 +0100

    add mongoose, connect to database, render index page

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 // dependancies
 const express       = require("express");
-const MongoClient   = require("mongodb").MongoClient;
 const bodyParser    = require("body-parser");
-
+const mongoose      = require('mongoose');
 const app           = express();
 
-const port = 8000;
+const port = 3000;
+
+mongoose.connect("mongodb://localhost:27017/node-blog");
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.get("/", (req, res) => {
+    res.render('index');
+});
 
 app.listen(port, () => {
     console.log('We are live on ' + port);
