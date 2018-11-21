2d9dfe75944052163781803758483ef5de67f258
sylvestertarnowski <sylwek.tarnowski@gmail.com>
2018-11-21 08:25:16 +0100

    mongoose add

--- app.js
+++ app.js
@@ -2,14 +2,20 @@
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const mongoose      = require('mongoose');
+const bodyParser    = require('body-parser');
+
 const app           = express();
 
 const port = 3000;
 
 mongoose.connect("mongodb://localhost:27017/node-blog");
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
+
+//Mongoose schema setup
 
 app.get("/", (req, res) => {
     res.render('index');
