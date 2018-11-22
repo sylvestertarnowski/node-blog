26c35ededa6f0f1805ffbeb41d2d24569005d1ce
sylvestertarnowski <sylwek.tarnowski@gmail.com>
2018-11-22 05:52:52 +0100

    Add blogs get and post routes, add blog schema

--- app.js
+++ app.js
@@ -2,13 +2,12 @@
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const mongoose      = require('mongoose');
-const bodyParser    = require('body-parser');
 
 const app           = express();
 
 const port = 3000;
 
-mongoose.connect("mongodb://localhost:27017/node-blog");
+mongoose.connect("mongodb://localhost:27017/node-blog", {useNewUrlParser: true});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -17,8 +16,27 @@ app.set('view engine', 'html');
 
 //Mongoose schema setup
 
+var blogSchema = new mongoose.Schema({ body: String });
+var Blog = mongoose.model('Blog', blogSchema);
+
+//Routes
+
 app.get("/", (req, res) => {
     res.render('index');
+});
+
+app.get("/blogs", (req, res) => {
+    res.send('blogs');
+});
+
+app.post("/blogs", (req, res) => {
+    Blog.create(req.body.blog, (err, newBlog) => {
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect("/blogs");
+        }
+    });
 });
 
 app.listen(port, () => {
