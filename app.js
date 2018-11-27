337daf3148823a1ee2b383a54e56fbfbc351687c
sylvestertarnowski <sylwek.tarnowski@gmail.com>
2018-11-27 06:02:46 +0100

    Add style.css, change the image size for individual blog posts

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ mongoose.connect("mongodb://localhost:27017/node-blog", { useNewUrlParser: true 
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressSanitizer());
 app.set('view engine', 'ejs');
 
 //Mongoose schema setup
